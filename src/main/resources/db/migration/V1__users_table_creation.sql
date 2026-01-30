6eeaba79e49c92d28d9bc360f4238f70bc64d947
Andrii Zaika <lytiy7404@gmail.com>
2026-01-30 19:30:28 +0100

    Add `currency_code` column to `users` table for storing user currency preferences

--- src/main/resources/db/migration/V1__users_table_creation.sql
+++ src/main/resources/db/migration/V1__users_table_creation.sql
@@ -1,13 +1,14 @@
 CREATE TABLE users
 (
-    id           BIGINT AUTO_INCREMENT NOT NULL,
-    full_name    VARCHAR(128)          NOT NULL,
-    password     VARCHAR(256)          NOT NULL,
-    email        VARCHAR(128)          NOT NULL,
-    avatar_url   VARCHAR(256),
-    is_activated BOOLEAN               NOT NULL,
-    created_at   datetime              NOT NULL,
-    updated_at   datetime              NOT NULL,
+    id            BIGINT AUTO_INCREMENT NOT NULL,
+    full_name     VARCHAR(128)          NOT NULL,
+    password      VARCHAR(256)          NOT NULL,
+    email         VARCHAR(128)          NOT NULL,
+    avatar_url    VARCHAR(256),
+    currency_code VARCHAR(3)            NOT NULL,
+    is_activated  BOOLEAN               NOT NULL,
+    created_at    datetime              NOT NULL,
+    updated_at    datetime              NOT NULL,
     CONSTRAINT pk_users PRIMARY KEY (id),
     CONSTRAINT uc_users_email UNIQUE (email)
 );
